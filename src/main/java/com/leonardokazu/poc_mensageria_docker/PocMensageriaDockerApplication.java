b523da425d22dfc2beeb56fcd1270dd3baa93a61
Leonardo Kazu <kazuyoshikad@gmail.com>
2025-10-26 10:39:52 -0300

    feat: Commit inicial.

---
+++ src/main/java/com/leonardokazu/poc_mensageria_docker/PocMensageriaDockerApplication.java
@@ -0,0 +1,13 @@
+package com.leonardokazu.poc_mensageria_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMensageriaDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMensageriaDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardokazu/poc_mensageria_docker/PocMensageriaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardokazu.poc_mensageria_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMensageriaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
