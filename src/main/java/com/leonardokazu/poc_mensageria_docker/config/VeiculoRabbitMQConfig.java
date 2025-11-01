b18011657a54959a1b460113dab1223d6793abb1
Leonardo Kazu <kazuyoshikad@gmail.com>
2025-11-01 01:30:23 -0300

    feat: Adicionar dependencia de validação e corrigir configs do RabbitMQ.

--- src/main/java/com/leonardokazu/poc_mensageria_docker/config/VeiculoRabbitMQConfig.java
+++ src/main/java/com/leonardokazu/poc_mensageria_docker/config/VeiculoRabbitMQConfig.java
@@ -4,28 +4,35 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class VeiculoRabbitMQConfig {
-    public static final String EXCHANGE_VEICULOS = "veiculo";
-    public static final String ROUTING_KEY_VEICULOS = "cadastrar.veiculo";
-    public static final String QUEUE_VEICULOS = "cadastrar.veiculo.queue";
+    public static final String EXCHANGE_VEICULO = "veiculo";
+    public static final String ROUTING_KEY_CADASTRAR_VEICULO = "cadastrar.veiculo";
+    public static final String QUEUE_VEICULO = "cadastrar.veiculo.queue";
 
     @Bean
     public DirectExchange veiculoExchange(){
-        return new DirectExchange(EXCHANGE_VEICULOS);
+        return new DirectExchange(EXCHANGE_VEICULO);
     }
 
     @Bean
     public Queue veiculoQueue(){
-        return new Queue(QUEUE_VEICULOS, true);
+        return new Queue(QUEUE_VEICULO, true);
     }
 
     @Bean
     public Binding bindVeiculo(){
-        return BindingBuilder.bind(veiculoQueue()).to(veiculoExchange()).with(ROUTING_KEY_VEICULOS);
+        return BindingBuilder.bind(veiculoQueue()).to(veiculoExchange()).with(ROUTING_KEY_CADASTRAR_VEICULO);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 
 }
